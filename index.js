fb3fb057a08ae3fe0c7c5f51ef3bc8fcf63b0846
NahomKibreab <nknahom@gmail.com>
2021-01-21 23:31:27 -0700

    EJS Passing Data to templates

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render('home', { num });
 });
 
 app.listen('3000', () => {
