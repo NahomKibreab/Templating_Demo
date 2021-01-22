08bb100bdf25f067748016735b1234998ff9eabd
NahomKibreab <nknahom@gmail.com>
2021-01-22 00:04:15 -0700

    Conditionals in EJS

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/rand', (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1;
-  res.render('home', { num });
+  res.render('random', { num });
 });
 
 app.get('/r/:subreddit', (req, res) => {
